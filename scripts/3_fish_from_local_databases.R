97a8638d7f54b0504892a8a0ef9e1806f596942d
cpavloud <cpavloud@hcmr.gr>
2023-09-22 16:06:46 +0200

    update fish from local

--- scripts/3_fish_from_local_databases.R
+++ scripts/3_fish_from_local_databases.R
@@ -59,12 +59,55 @@ packageVersion("rredlist")
 ################### AND FROM THE TREMATOD DATABASE OF NHM ######################
 ################################################################################
 
+#import the information on the MEOW provinces
+MEOW_provinces <- read.csv("Spalding_provinces.txt", sep = '\t', header = TRUE)
+
+#import the local Trematoda database
+Trematoda_local <- read.csv("Trematoda_local_database.txt", sep = '\t', header = TRUE)
+Trematoda_local <- merge(MEOW_provinces, Trematoda_local, by=c('PROV_CODE'='PROV_CODE'), all.y=TRUE)
+Trematoda_local <- select(Trematoda_local, -RLM_CODE, -REALM, -Lat_Zone)
+#sanity check
+colSums(is.na(Trematoda_local))
+
+#import the NHM Trematoda database
+Trematoda_NHM <- read.csv("NHM_trematoda_host_species_province.txt", sep = '\t', header = TRUE)
+Trematoda_NHM <- select(Trematoda_NHM, -Authority, -Location)
+Trematoda_NHM <- merge(MEOW_provinces, Trematoda_NHM, by=c('PROVINCE'='PROVINCE'), all.y=TRUE)
+Trematoda_NHM <- select(Trematoda_NHM, -RLM_CODE, -REALM, -Lat_Zone)
+#sanity check
+colSums(is.na(Trematoda_NHM))
+
+#merge the Trematoda data frames
+Trematoda <- rbind(Trematoda_local, Trematoda_NHM)
+#sanity check
+colSums(is.na(Trematoda))
+Trematoda <- unique(Trematoda)
+
+#import the local Copepoda database
+Copepoda_local <- read.csv("Copepoda_local_database.txt", sep = '\t', header = TRUE)
+Copepoda_local <- merge(MEOW_provinces, Copepoda_local, by=c('PROVINCE'='PROVINCE'), all.y=TRUE)
+Copepoda_local <- select(Copepoda_local, -RLM_CODE, -REALM, -Lat_Zone)
+
+#sanity check
+colSums(is.na(Copepoda_local))
+Copepoda_local <- unique(Copepoda_local)
+
+################################################################################
+################################ COMBINE FISH HOSTS ############################
+################################################################################
+
+Copepoda_host <- select(Copepoda_local, scientificName_Host)
+Copepoda_host <- unique(Copepoda_host)
+Trematoda_host <- select(Trematoda, scientificName_Host)
+Trematoda_host <- unique(Trematoda_host)
+
+Fish_Hosts <- rbind(Copepoda_host, Trematoda_host)
+Fish_Hosts <- unique(Fish_Hosts)
 
 
+#Save the workspace
+save.image(file = "Fish_Hosts.RData")
 
-
-
-
-
-
-
+################################################################################
+################################################################################
+################################################################################
